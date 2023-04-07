3f01a69d79dcd9723aeb39ba3d46716e3380522e
kevin tang <kevin26428@gmail.com>
2023-04-06 20:25:44 -0700

    initilize firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import { initializeApp } from "firebase/app";
 
+// Initialize Firebase
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyBGaSbdoHu5IW78nDf58nA_NtjLCMjbfH4",
+  authDomain: "pomofriend.firebaseapp.com",
+  projectId: "pomofriend",
+  storageBucket: "pomofriend.appspot.com",
+  messagingSenderId: "86326119163",
+  appId: "1:86326119163:web:5fad4d4ed4bfe04bd23e5e"
+});
+
+/**
+ * 
+ * key value
+ * roomId: {
+ *   startedAt: ISO timestamp
+ *   type: focus | short break | long break
+ *   focusCount: [0 - 3]
+ * }
+ */
 function App() {
+  console.log(firebaseApp)
+  const [roomId, setRoomId] = useState("roomId101123");
+  const [timeLeft, setTimeLeft] = useState(100);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{"current room: " + roomId}</h1>
+      <button>Start</button>
+      <h1>{"time left: " + timeLeft}</h1>
     </div>
   );
 }
