f85ef8cf5c80ea151ee68a98f9e6cfba2f49a354
kevin tang <kevin26428@gmail.com>
2023-05-12 15:46:35 -0700

    add firebase analytics

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,17 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
-
-export const app = initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyBGaSbdoHu5IW78nDf58nA_NtjLCMjbfH4",
   authDomain: "pomofriend.firebaseapp.com",
+  databaseURL: "https://pomofriend-default-rtdb.firebaseio.com",
   projectId: "pomofriend",
   storageBucket: "pomofriend.appspot.com",
   messagingSenderId: "86326119163",
   appId: "1:86326119163:web:5fad4d4ed4bfe04bd23e5e",
-  databaseURL: "https://pomofriend-default-rtdb.firebaseio.com/",
-});
+  measurementId: "G-KLJ25XSP2J"
+};
 
+export const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
+export const analytics = getAnalytics(app);
